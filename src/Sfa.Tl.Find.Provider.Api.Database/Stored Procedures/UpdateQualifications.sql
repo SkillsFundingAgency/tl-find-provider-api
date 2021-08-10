7454b57ed417550b51f5284decb9d28fbf847296
Mike Wild <wildconsultingltd@googlemail.com>
2021-08-10 16:20:58 +0100

    Add DB types and procs for Qualifications

---
+++ src/Sfa.Tl.Find.Provider.Api.Database/Stored Procedures/UpdateQualifications.sql
@@ -0,0 +1,33 @@
+﻿CREATE PROCEDURE [dbo].[UpdateQualifications]
+  @data [dbo].[QualificationDataTableType] READONLY
+AS
+	SET NOCOUNT ON;
+
+	MERGE INTO [dbo].[Qualification] AS t
+	USING @data AS s
+	ON
+	(
+	  t.[Id] = s.[Id]
+	)
+	WHEN MATCHED 
+		 AND (t.[Id] <> s.[Id] 
+			  OR t.[Name] <> s.[Name]
+			  OR t.[IsDeleted] = 0)
+	THEN UPDATE SET
+	  t.[Id]  = s.[Id],
+	  t.[Name] = s.[Name],
+	  t.[IsDeleted] = 0
+
+	WHEN NOT MATCHED BY TARGET THEN INSERT
+	(
+	  [Id],
+	  [Name]
+	)
+	VALUES
+	(
+	  s.[Id],
+	  s.[Name]
+	)
+	WHEN NOT MATCHED BY SOURCE THEN 
+	UPDATE SET
+	  t.[IsDeleted] = 1; --Soft delete
