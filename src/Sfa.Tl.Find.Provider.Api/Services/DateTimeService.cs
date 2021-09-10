7f1021731cc0e1b1ed16ba29d08bd34e1d5f05fb
Mike Wild <wildconsultingltd@googlemail.com>
2021-09-10 11:15:09 +0100

    Remove unused properties from DateTimeService

--- src/Sfa.Tl.Find.Provider.Api/Services/DateTimeService.cs
+++ src/Sfa.Tl.Find.Provider.Api/Services/DateTimeService.cs
@@ -7,8 +7,6 @@ namespace Sfa.Tl.Find.Provider.Api.Services
     {
         public DateTime Now => DateTime.Now;
         public DateTime UtcNow => DateTime.UtcNow;
-        public DateTime MinValue => DateTime.MinValue;
-        public DateTime MaxValue => DateTime.MaxValue;
         public DateTime Today => DateTime.Today;
     }
 }
