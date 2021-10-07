c2402c88da473b02f766614c434b3118ae3f32b6
Mike Wild <wildconsultingltd@googlemail.com>
2021-10-07 11:19:27 +0100

    Code tidying

--- src/Sfa.Tl.Find.Provider.Api/Startup.cs
+++ src/Sfa.Tl.Find.Provider.Api/Startup.cs
@@ -6,7 +6,6 @@ using System.Reflection;
 using AspNetCoreRateLimit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
