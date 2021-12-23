a2f2321a62a29cd8c6721e81f8568bd5491bdc20
Mike Wild <wildconsultingltd@googlemail.com>
2021-12-23 10:35:41 +0000

    Add jittered backoff to dapper retry policy

--- src/Sfa.Tl.Find.Provider.Api/Extensions/PollyRegistryExtensions.cs
+++ src/Sfa.Tl.Find.Provider.Api/Extensions/PollyRegistryExtensions.cs
@@ -3,6 +3,7 @@ using System.ComponentModel;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 using Polly;
+using Polly.Contrib.WaitAndRetry;
 using Polly.Registry;
 using Sfa.Tl.Find.Provider.Api.Data;
 using Sfa.Tl.Find.Provider.Api.Models;
@@ -13,37 +14,35 @@ namespace Sfa.Tl.Find.Provider.Api.Extensions;
 
 public static class PollyRegistryExtensions
 {
-    private const int RetryCount = 4;
-    private static readonly Random Random = new();
-
     public static IPolicyRegistry<string> AddDapperRetryPolicy(this IPolicyRegistry<string> policyRegistry)
     {
+        var backoff = Backoff.DecorrelatedJitterBackoffV2(
+            TimeSpan.FromSeconds(1),
+            7,
+            seed: 100);
+
         var retryPolicy = Policy
             .Handle<SqlException>(SqlServerTransientExceptionDetector.ShouldRetryOn)
             .Or<SqlException>(SqlServerTransientExceptionDetector.CouldNotOpenConnection)
             .Or<TimeoutException>()
             .OrInner<Win32Exception>(SqlServerTransientExceptionDetector.ShouldRetryOn)
             .WaitAndRetryAsync(
-                RetryCount,
-                currentRetryNumber => 
-                    TimeSpan.FromSeconds(Math.Pow(1.5, currentRetryNumber - 1)) 
-                    + TimeSpan.FromMilliseconds(Random.Next(0, 100)),
+                backoff,
                 (exception,
                     sleepDuration,
-                    retryNumber,
                     context) =>
                 {
                     if (!context.TryGetLogger(out var logger)) return;
-                        
+
                     if (exception != null)
                     {
                         logger.LogWarning(exception, "An error occurred when calling the database. Retrying after {sleepDuration}. Retry {retryAttempt} for {policyKey}",
-                            sleepDuration, retryNumber, context.PolicyKey);
+                            sleepDuration, context.Count, context.PolicyKey);
                     }
                     else
                     {
                         logger.LogWarning("A non success code was received on retry {RetryAttempt} for {PolicyKey}",
-                            retryNumber, context.PolicyKey);
+                            context.Count, context.PolicyKey);
                     }
                 })
             .WithPolicyKey(Constants.DapperRetryPolicyName);
