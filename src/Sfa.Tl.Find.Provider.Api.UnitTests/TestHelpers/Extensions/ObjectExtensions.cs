74a5e47b950b37216a04c8035e0948aa04f77638
Mike Wild <wildconsultingltd@googlemail.com>
2022-01-25 14:08:12 +0000

    Code tidying

--- src/Sfa.Tl.Find.Provider.Api.UnitTests/TestHelpers/Extensions/ObjectExtensions.cs
+++ src/Sfa.Tl.Find.Provider.Api.UnitTests/TestHelpers/Extensions/ObjectExtensions.cs
@@ -3,29 +3,27 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Reflection;
 
-namespace Sfa.Tl.Find.Provider.Api.UnitTests.TestHelpers.Extensions
+namespace Sfa.Tl.Find.Provider.Api.UnitTests.TestHelpers.Extensions;
+
+public static class ObjectExtensions
 {
-    public static class ObjectExtensions
+    public static ExpandoObject ToExpandoObject(this object value)
     {
-        public static ExpandoObject ToExpandoObject(this object value)
+        ArgumentNullException.ThrowIfNull(value);
+
+        var obj = new ExpandoObject() as IDictionary<string, object>;
+
+        foreach (var property in value.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
         {
-            ArgumentNullException.ThrowIfNull(value);
-
-            var obj = new ExpandoObject() as IDictionary<string, object>;
-
-            foreach (var property in value.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
-            {
-                obj.Add(property.Name, property.GetValue(value, null));
-            }
-
-            return (ExpandoObject)obj;
+            obj.Add(property.Name, property.GetValue(value, null));
         }
 
-        public static int GetIsAdditionalDataValueFromAnonymousType(this object o)
-        {
-            dynamic z = o.ToExpandoObject();
-            return (int)z.isAdditionalData;
-        }
+        return (ExpandoObject)obj;
+    }
 
+    public static int GetIsAdditionalDataValueFromAnonymousType(this object o)
+    {
+        dynamic z = o.ToExpandoObject();
+        return (int)z.isAdditionalData;
     }
 }
