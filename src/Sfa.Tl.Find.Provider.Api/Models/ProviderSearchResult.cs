1d26cc6542852d465dd2f1350d85c02d88954267
Mike Wild <wildconsultingltd@googlemail.com>
2022-02-25 10:24:34 +0000

    Make API return additional address and journey details

--- src/Sfa.Tl.Find.Provider.Api/Models/ProviderSearchResult.cs
+++ src/Sfa.Tl.Find.Provider.Api/Models/ProviderSearchResult.cs
@@ -16,18 +16,14 @@ public class ProviderSearchResult
     [JsonIgnore]
     public string LocationName { get; init; }
     public string Postcode { get; init; }
-    [JsonIgnore]
     public string AddressLine1 { get; init; }
-    [JsonIgnore]
     public string AddressLine2 { get; init; }
     public string Town { get; init; }
-    [JsonIgnore]
     public string County { get; init; }
     public string Email { get; init; }
     public string Telephone { get; init; }
     public string Website { get; init; }
     public double Distance { get; init; }
-    [JsonIgnore]
     public string JourneyToLink { get; set; }
 
     public ICollection<DeliveryYear> DeliveryYears { get; init; } = new List<DeliveryYear>();
