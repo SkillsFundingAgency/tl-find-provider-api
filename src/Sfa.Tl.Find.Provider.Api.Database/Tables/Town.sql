331e34ad032ce0004d0a1d4714ff4f586468e8af
Mike Wild <wildconsultingltd@googlemail.com>
2022-03-09 11:44:46 +0000

    Add town table, index and Id list type

---
+++ src/Sfa.Tl.Find.Provider.Api.Database/Tables/Town.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Town]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[TownName] [varchar](256) NOT NULL,
+	[CountyName] [varchar](256) NULL,
+	[LocalAuthorityName] [varchar](256) NULL,
+	[Latitude] DECIMAL(9, 6) NOT NULL,
+	[Longitude] DECIMAL(9, 6) NOT NULL,
+    CONSTRAINT [PK_Town] PRIMARY KEY ([Id])
+)
