6a5c3b3787648ce6210e97900d5bda327d9e03b7
Mike Wild <wildconsultingltd@googlemail.com>
2022-03-11 17:03:36 +0000

    Implement Save in TownRepository

--- src/Sfa.Tl.Find.Provider.Api/Data/TownRepository.cs
+++ src/Sfa.Tl.Find.Provider.Api/Data/TownRepository.cs
@@ -1,52 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Polly.Registry;
+using Sfa.Tl.Find.Provider.Api.Extensions;
 using Sfa.Tl.Find.Provider.Api.Interfaces;
 using Sfa.Tl.Find.Provider.Api.Models;
 
-namespace Sfa.Tl.Find.Provider.Api.Data
+namespace Sfa.Tl.Find.Provider.Api.Data;
+
+public class TownRepository : ITownRepository
 {
-    public class TownRepository : ITownRepository
+    private readonly IDbContextWrapper _dbContextWrapper;
+    private readonly ILogger<TownRepository> _logger;
+    private readonly IReadOnlyPolicyRegistry<string> _policyRegistry;
+
+    public TownRepository(
+        IDbContextWrapper dbContextWrapper,
+        IReadOnlyPolicyRegistry<string> policyRegistry,
+        ILogger<TownRepository> logger)
     {
-        private readonly IDbContextWrapper _dbContextWrapper;
-        private readonly ILogger<TownRepository> _logger;
-        private readonly IReadOnlyPolicyRegistry<string> _policyRegistry;
+        _dbContextWrapper = dbContextWrapper ?? throw new ArgumentNullException(nameof(dbContextWrapper));
+        _policyRegistry = policyRegistry ?? throw new ArgumentNullException(nameof(policyRegistry));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
 
-        public TownRepository(
-            IDbContextWrapper dbContextWrapper,
-            IReadOnlyPolicyRegistry<string> policyRegistry,
-            ILogger<TownRepository> logger)
+    public async Task<bool> HasAny()
+    {
+        using var connection = _dbContextWrapper.CreateConnection();
+
+        var result = await _dbContextWrapper.ExecuteScalarAsync<int>(
+            connection,
+            "SELECT COUNT(1) " +
+            "WHERE EXISTS (SELECT 1 FROM dbo.Town)");
+
+        return result != 0;
+    }
+
+    public async Task Save(IEnumerable<Town> towns)
+    {
+        try
         {
-            _dbContextWrapper = dbContextWrapper ?? throw new ArgumentNullException(nameof(dbContextWrapper));
-            _policyRegistry = policyRegistry ?? throw new ArgumentNullException(nameof(policyRegistry));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            var (retryPolicy, context) = _policyRegistry.GetRetryPolicy(_logger);
+
+            await retryPolicy
+                .ExecuteAsync(async _ =>
+                        await PerformSave(towns),
+                    context);
+
         }
-
-        public async Task<bool> HasAny()
+        catch (Exception ex)
         {
-            using var connection = _dbContextWrapper.CreateConnection();
+            _logger.LogError(ex, "An error occurred when saving towns");
+            throw;
+        }
+    }
 
-            var result = await _dbContextWrapper.ExecuteScalarAsync<int>(
+    private async Task PerformSave(IEnumerable<Town> towns)
+    {
+        using var connection = _dbContextWrapper.CreateConnection();
+        connection.Open();
+
+        using var transaction = _dbContextWrapper.BeginTransaction(connection);
+        var updateResult = await _dbContextWrapper
+            .QueryAsync<(string Change, int ChangeCount)>(
                 connection,
-                "SELECT COUNT(1) " +
-                "WHERE EXISTS (SELECT 1 FROM dbo.Town)");
+                "UpdateTowns",
+                new
+                {
+                    data = towns.AsTableValuedParameter(
+                        "dbo.TownDataTableType")
+                },
+                transaction,
+                commandType: CommandType.StoredProcedure);
 
-            return result != 0;
-        }
+        _logger.LogChangeResults(updateResult, nameof(QualificationRepository),
+            nameof(towns));
 
-        public async Task Save(IEnumerable<Town> towns)
-        {
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "An error occurred when saving towns");
-                throw;
-            }
-        }
+        transaction.Commit();
     }
 }
