e014c41254e2df69c3faaddedd46036457365105
Mike Wild <wildconsultingltd@googlemail.com>
2022-04-19 15:31:35 +0100

    Search proc - look up town and check if any qualification ids

--- src/Sfa.Tl.Find.Provider.Api.Database/Stored Procedures/SearchProviders.sql
+++ src/Sfa.Tl.Find.Provider.Api.Database/Stored Procedures/SearchProviders.sql
@@ -13,16 +13,23 @@ AS
 	DECLARE @fromLocation GEOGRAPHY = geography::Point(@fromLatitude, @fromLongitude, 4326);
 
 	DECLARE @allQualificationIds TABLE ([Id] INT)
+	DECLARE @hasQualificationIds BIT = 0
 
-	INSERT INTO @allQualificationIds
-	SELECT [QualificationId] 
-	FROM @routeIds r
-	INNER JOIN [RouteQualification] rq
-	ON	rq.[RouteId] = r.[Id]
-	UNION
-	SELECT [Id] 
-	FROM @qualificationIds;
-	
+	IF(EXISTS(SELECT 1 from @routeIds) OR 
+	   EXISTS(SELECT 1 from @qualificationIds))
+	BEGIN
+		SET @hasQualificationIds = 1
+
+		INSERT INTO @allQualificationIds
+		SELECT [QualificationId] 
+		FROM @routeIds r
+		INNER JOIN [RouteQualification] rq
+		ON	rq.[RouteId] = r.[Id]
+		UNION
+		SELECT [Id]
+		FROM @qualificationIds
+	END;
+		
 	WITH ProvidersCTE AS (
 		SELECT	p.[Id],
 				p.[UkPrn],
@@ -36,6 +43,7 @@ AS
 		WHERE	p.[IsDeleted] = 0
 		  --If not merging additional data, have to make sure we only include non-additional data
 		  AND	(@includeAdditionalData = 1 OR (@includeAdditionalData = 0 AND p.[IsAdditionalData] = 0))),
+
 	NearestLocationsCTE AS (
 		SELECT	p.[UkPrn],
 				p.[Name] AS [ProviderName],
@@ -46,9 +54,9 @@ AS
 				l.[AddressLine2],
 				l.[Town],
 				l.[County],
-				COALESCE(NULLIF(l.[Email],''), p.[Email]) AS [Email],
-				COALESCE(NULLIF(l.[Telephone],''), p.[Telephone]) AS [Telephone],
-				COALESCE(NULLIF(l.[Website],''), p.[Website]) AS [Website],
+				COALESCE(NULLIF(l.[Email], ''), p.[Email]) AS [Email],
+				COALESCE(NULLIF(l.[Telephone], ''), p.[Telephone]) AS [Telephone],
+				COALESCE(NULLIF(l.[Website], ''), p.[Website]) AS [Website],
 				l.[Location].STDistance(@fromLocation) / 1609.3399999999999E0 AS [Distance] --(Miles)
 		FROM	[ProvidersCTE] p
 		INNER JOIN	[dbo].[Location] l
@@ -68,14 +76,27 @@ AS
 				ON		r.[Id] = rq.[RouteId]
 				  AND	r.[IsDeleted] = 0
 				WHERE	lq.[LocationId] = l.[Id]
-				  AND	((NOT EXISTS(SELECT [Id] FROM @allQualificationIds WHERE [Id] <> 0)
-						   OR q.[Id] IN (SELECT [Id] FROM @allQualificationIds))
-						))
+				  AND	(@hasQualificationIds = 0
+						 OR q.[Id] IN (SELECT [Id] FROM @allQualificationIds)))
 		ORDER BY [Distance],
 				 p.[Name],
 				 l.[Name]
 		OFFSET @page * @pageSize ROWS
-		FETCH NEXT @pageSize ROWS ONLY)
+		FETCH NEXT @pageSize ROWS ONLY),
+		
+		TownNamesCTE AS (
+			SELECT	[LocationId],
+					COALESCE(t.[Name], l.[Town]) AS [TownName]
+			FROM NearestLocationsCTE l		
+			OUTER APPLY (SELECT Top(1) [Name] 
+						 FROM [dbo].[Town]
+						 WHERE [Name] = l.[Town]
+						 OR [Name] = REPLACE(l.[Town], ' ', '-')
+						 ) t
+			GROUP BY [LocationId],
+					 l.[Town],
+					 t.[Name])
+
 		--Step 2 - add in the qualifications (no filter for qualifications - return all for selected locations)
 		SELECT 	[UkPrn],
 				[ProviderName],
@@ -83,7 +104,7 @@ AS
 				[LocationName],
 				[AddressLine1],
 				[AddressLine2],
-				[Town],
+				t.[TownName],
 				[County],
 				[Email],
 				[Telephone],
@@ -97,6 +118,8 @@ AS
 		FROM NearestLocationsCTE l
 		INNER JOIN	[dbo].[LocationQualification] lq
 		ON		lq.[LocationId] = l.[LocationId]
+		INNER JOIN [TownNamesCTE] t
+		ON		t.[LocationId] = l.[LocationId]
 		INNER JOIN	[dbo].[Qualification] q
 		ON		q.[Id] = lq.[QualificationId]
 		  AND	q.[IsDeleted] = 0
