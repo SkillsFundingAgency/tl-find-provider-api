40fe05cbf8de807fc2bf5873e6ae4a82ca2d8a57
Mike Wild <wildconsultingltd@googlemail.com>
2022-04-27 17:53:19 +0100

    Remove HMAC attribute for employer registration poc

--- src/Sfa.Tl.Find.Provider.Api/Controllers/EmployerRegistrationController.cs
+++ src/Sfa.Tl.Find.Provider.Api/Controllers/EmployerRegistrationController.cs
@@ -13,6 +13,7 @@ namespace Sfa.Tl.Find.Provider.Api.Controllers;
 
 [ApiController]
 [ApiVersion("2.0")]
+//[HmacAuthorization]
 [Route("api/v{version:apiVersion}/[controller]")]
 [ResponseCache(NoStore = true, Duration = 0, Location = ResponseCacheLocation.None)]
 public class EmployerRegistrationController : ControllerBase
