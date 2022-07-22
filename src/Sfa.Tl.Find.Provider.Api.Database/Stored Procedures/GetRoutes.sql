aa80285b59b656846d0a3fb2bed2b12c139cf6fa
Mike Wild <wildconsultingltd@googlemail.com>
2022-07-22 16:19:17 +0100

    Fix qualification counts in GetRoutes

--- src/Sfa.Tl.Find.Provider.Api.Database/Stored Procedures/GetRoutes.sql
+++ src/Sfa.Tl.Find.Provider.Api.Database/Stored Procedures/GetRoutes.sql
@@ -12,13 +12,20 @@ AS
 		WHERE	p.[IsDeleted] = 0
 		  --If not merging additional data, have to make sure we only include non-additional data
 		  AND	(@includeAdditionalData = 1 OR (@includeAdditionalData = 0 AND p.[IsAdditionalData] = 0))),
+	
+	--Need to group here to remove multiple delivery years
+	LocationQualificationCTE AS (
+		SELECT [LocationId], [QualificationId]
+		FROM [dbo].[LocationQualification] 
+		GROUP BY [LocationId], [QualificationId]),
+
 	LocationsCTE AS (
 		SELECT	rq.[RouteId] AS [RouteId],
 				rq.[QualificationId] AS [QualificationId]
 		FROM	[ProvidersCTE] p
 		INNER JOIN	[dbo].[Location] l
 		ON		p.[Id] = l.[ProviderId]
-		INNER JOIN	[dbo].[LocationQualification] lq
+		INNER JOIN	[LocationQualificationCTE] lq
 		ON		lq.[LocationId] = l.[Id]
 		INNER JOIN	[dbo].[Qualification] q
 		ON		q.[Id] = lq.[QualificationId]
@@ -36,7 +43,7 @@ AS
 			r.[Name] AS [RouteName], 
 			q.[Id] AS [QualificationId], 
 			q.[Name] AS [QualificationName],
-			COUNT(q.[Id]) AS [NumberOfQualificationsOffered]
+			COUNT(cte.[QualificationId]) AS [NumberOfQualificationsOffered]
 	FROM [Route] r
 	LEFT JOIN	[RouteQualification] rq
 	ON		r.[Id] = rq.[RouteId]
