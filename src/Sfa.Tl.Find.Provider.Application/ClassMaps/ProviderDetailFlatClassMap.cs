649f5e21bb0268283c027a4bdd2c6839806da65a
Mike Wild <wildconsultingltd@googlemail.com>
2022-08-23 15:59:21 +0100

    Fix csv column class map

--- src/Sfa.Tl.Find.Provider.Application/ClassMaps/ProviderDetailFlatClassMap.cs
+++ src/Sfa.Tl.Find.Provider.Application/ClassMaps/ProviderDetailFlatClassMap.cs
@@ -10,13 +10,14 @@ public sealed class ProviderDetailFlatClassMap : ClassMap<ProviderDetailFlat>
         Map(p => p.UkPrn).Name("UKPRN");
         Map(p => p.ProviderName).Name("Provider Name");
         Map(p => p.Postcode).Name("Postcode");
+        Map(p => p.LocationName).Name("Location Name");
         Map(p => p.AddressLine1).Name("Address Line 1");
         Map(p => p.AddressLine2).Name(".Address Line 2");
         Map(p => p.Town).Name("Town");
         Map(p => p.County).Name("County");
         Map(p => p.Email).Name("Email");
-        Map(p => p.Telephone).Name(".Telephone");
-        Map(p => p.Website).Name(".Website");
+        Map(p => p.Telephone).Name("Telephone");
+        Map(p => p.Website).Name("Website");
         Map(p => p.Year).Name("Year of Delivery");
         Map(p => p.RouteName).Name("Route Name");
         Map(p => p.QualificationName).Name("Qualification Name");
