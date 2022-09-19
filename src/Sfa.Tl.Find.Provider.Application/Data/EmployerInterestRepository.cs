d479e9a01e68893b910be4ce9d56b855297bca5f
Mike Wild <wildconsultingltd@googlemail.com>
2022-09-19 13:08:48 +0100

    Add debugging in DeleteBefore

--- src/Sfa.Tl.Find.Provider.Application/Data/EmployerInterestRepository.cs
+++ src/Sfa.Tl.Find.Provider.Application/Data/EmployerInterestRepository.cs
@@ -59,6 +59,8 @@ public class EmployerInterestRepository : IEmployerInterestRepository
 
         using var transaction = _dbContextWrapper.BeginTransaction(connection);
 
+        _logger.LogInformation("{method} preparing to delete before {date}.", nameof(DeleteBefore), date);
+
         var result = await _dbContextWrapper.ExecuteAsync(
             connection,
             "DELETE dbo.EmployerInterest " +
@@ -66,7 +68,9 @@ public class EmployerInterestRepository : IEmployerInterestRepository
             _dynamicParametersWrapper.DynamicParameters,
             transaction);
         
+        _logger.LogInformation("{method} deleted {count} records.", nameof(DeleteBefore), result);
         transaction.Commit();
+        _logger.LogInformation("{method} commit complete.", nameof(DeleteBefore));
 
         return result;
     }
