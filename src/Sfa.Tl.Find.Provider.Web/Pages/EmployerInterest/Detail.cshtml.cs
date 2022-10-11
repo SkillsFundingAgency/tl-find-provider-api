ad81b5c149ca0bfc5b2220de0aebf85883ce070a
Mike Wild <wildconsultingltd@googlemail.com>
2022-10-11 09:12:10 +0100

    Code tidying in a few pages

--- src/Sfa.Tl.Find.Provider.Web/Pages/EmployerInterest/Detail.cshtml.cs
+++ src/Sfa.Tl.Find.Provider.Web/Pages/EmployerInterest/Detail.cshtml.cs
@@ -1,26 +1,25 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Sfa.Tl.Find.Provider.Application.Interfaces;
 
-namespace Sfa.Tl.Find.Provider.Web.Pages.EmployerInterest
+namespace Sfa.Tl.Find.Provider.Web.Pages.EmployerInterest;
+
+public class DetailModel : PageModel
 {
-    public class DetailModel : PageModel
+    public Application.Models.EmployerInterest? EmployerInterest { get; private set; }
+
+    private readonly ILogger<DetailModel> _logger;
+    private IEmployerInterestService _employerInterestService;
+
+    public DetailModel(
+        IEmployerInterestService employerInterestService,
+        ILogger<DetailModel> logger)
     {
-        public Application.Models.EmployerInterest? EmployerInterest { get; private set; }
+        _employerInterestService = employerInterestService ?? throw new ArgumentNullException(nameof(employerInterestService));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
 
-        private readonly ILogger<DetailModel> _logger;
-        private IEmployerInterestService _employerInterestService;
-
-        public DetailModel(
-            IEmployerInterestService employerInterestService,
-            ILogger<DetailModel> logger)
-        {
-            _employerInterestService = employerInterestService ?? throw new ArgumentNullException(nameof(employerInterestService));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        }
-
-        public async Task OnGet(int id)
-        {
-            EmployerInterest = await _employerInterestService.GetEmployerInterest(id);
-        }
+    public async Task OnGet(int id)
+    {
+        EmployerInterest = await _employerInterestService.GetEmployerInterest(id);
     }
 }
