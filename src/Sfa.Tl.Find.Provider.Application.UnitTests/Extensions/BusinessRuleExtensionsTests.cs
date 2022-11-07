a48e6bf29a2a260fee348c6447d563329d4b9a24
Mike Wild <wildconsultingltd@googlemail.com>
2022-11-07 12:41:48 +0000

    Suppress "New" for first 7 days

--- src/Sfa.Tl.Find.Provider.Application.UnitTests/Extensions/BusinessRuleExtensionsTests.cs
+++ src/Sfa.Tl.Find.Provider.Application.UnitTests/Extensions/BusinessRuleExtensionsTests.cs
@@ -35,9 +35,6 @@ public class BusinessRuleExtensionsTests
             CreatedOn = DateTime.Parse(createdDate),
             ModifiedOn = modifiedDate is not null ? DateTime.Parse(modifiedDate) : null
         };
-        //Expected result to be
-        //<2022 - 12 - 31 >, but found
-        //<2020 - 12 - 31 11:30:00 >.
 
         var result = target.InterestExpiryDate(retentionDays);
         result.Should().Be(expectedResultDate);
@@ -77,6 +74,33 @@ public class BusinessRuleExtensionsTests
         };
 
         var result = target.IsInterestNew(today);
+        result.Should().Be(expectedResult);
+    }
+    
+    [Theory(DisplayName = nameof(BusinessRuleExtensions.IsInterestNew) + " Service Start Date Data Tests")]
+    [InlineData(null, "2022-12-01 11:30",  "2022-12-05", true)]
+    [InlineData(null, "2022-12-01 11:30", "2022-12-07", true)]
+    [InlineData(null, "2022-12-01 11:30",  "2022-12-08", false)]
+    [InlineData("2022-12-01", "2022-12-01 11:30", "2022-12-05", false)]
+    [InlineData("2022-12-01", "2022-12-07 11:30", "2022-12-07", false)]
+    [InlineData("2022-12-01", "2022-12-02 11:30", "2022-12-08", false)]
+    [InlineData("2022-12-01", "2022-12-08 11:30", "2022-12-08", true)]
+    [InlineData("2022-12-01", "2022-12-09 11:30", "2022-12-09", true)]
+    public void EmployerInterestSummary_IsInterestNew_With_Service_Start_Date_Data_Tests(
+        string serviceStartDateString, string createdDate, string currentDate, bool expectedResult)
+    {
+        var today = DateTime.Parse(currentDate);
+
+        var serviceStartDate = serviceStartDateString is not null 
+            ? DateOnly.Parse(serviceStartDateString)
+            : null as DateOnly?;
+
+        var target = new EmployerInterestSummary
+        {
+            CreatedOn = DateTime.Parse(createdDate),
+        };
+
+        var result = target.IsInterestNew(today, serviceStartDate: serviceStartDate);
         result.Should().Be(expectedResult);
     }
 }
