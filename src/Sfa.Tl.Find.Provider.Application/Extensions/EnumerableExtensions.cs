82ddeab44dd6c08b84a28d3b86bf607467aa76c7
Mike Wild <wildconsultingltd@googlemail.com>
2022-11-21 10:37:46 +0000

    Tidy up EnumerableExtensions

--- src/Sfa.Tl.Find.Provider.Application/Extensions/EnumerableExtensions.cs
+++ src/Sfa.Tl.Find.Provider.Application/Extensions/EnumerableExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Data;
+﻿using System.Data;
 using System.Reflection;
 using Dapper.Contrib.Extensions;
 
@@ -22,33 +21,28 @@ public static class EnumerableExtensions
         }
         else
         {
-            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
-            var readableProperties = properties.Where(w => 
-                    w.CanRead && 
-                    !w.HasAttribute<WriteAttribute>() //Not to be written via Dapper
+            var properties = typeof(T)
+                .GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                .Where(w =>
+                        w.CanRead &&
+                        !w.HasAttribute<WriteAttribute>() //Not to be written via Dapper
             ).ToArray();
 
-            var columnNames = (orderedColumnNames ?? readableProperties.Select(s => s.Name)).ToArray();
+            var columnNames = (orderedColumnNames 
+                               ?? properties.Select(s => s.Name)).ToArray();
             foreach (var name in columnNames)
             {
-                if (name == "ContactPreferenceType")
-                {
-                    var propertyInfo = readableProperties.Single(s => s.Name.Equals(name));
-                    var column = new DataColumn
-                    {
-                        ColumnName = name,
-                        DataType = propertyInfo.PropertyType.Name.Contains("Nullable") ? typeof(string) : propertyInfo.PropertyType
-                    };
-                    dataTable.Columns.Add(column);
-                }
-                else
-                dataTable.Columns.Add(name, readableProperties.Single(s => s.Name.Equals(name)).PropertyType);
+                var propertyInfo = properties.Single(s => s.Name.Equals(name));
+                dataTable.Columns.Add(name,
+                    propertyInfo.PropertyType.Name.Contains("Nullable") 
+                        ? typeof(string) 
+                        : propertyInfo.PropertyType);
             }
 
             foreach (var obj in data)
             {
                 dataTable.Rows.Add(
-                    columnNames.Select(s => readableProperties.Single(s2 => s2.Name.Equals(s)).GetValue(obj))
+                    columnNames.Select(s => properties.Single(s2 => s2.Name.Equals(s)).GetValue(obj))
                         .ToArray());
             }
         }
