c0a7edfcb8a6e1100214ee5cf883de3dab6deb32
Mike Wild <wildconsultingltd@googlemail.com>
2023-01-17 17:18:00 +0000

    Add tests for Error page, remove unused property

---
+++ src/Sfa.Tl.Find.Provider.Web.UnitTests/Builders/ErrorModelBuilder.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Sfa.Tl.Find.Provider.Web.Pages;
+
+namespace Sfa.Tl.Find.Provider.Web.UnitTests.Builders;
+public class ErrorModelBuilder
+{
+    public ErrorModel Build(
+            ILogger<ErrorModel>? logger = null,
+            PageContext? pageContext = null)
+    {
+        pageContext ??= new PageContextBuilder()
+            .Build();
+
+        logger ??= Substitute.For<ILogger<ErrorModel>>();
+
+        var pageModel = new ErrorModel(
+            logger)
+        {
+            PageContext = pageContext
+        };
+
+        return pageModel;
+    }
+}
