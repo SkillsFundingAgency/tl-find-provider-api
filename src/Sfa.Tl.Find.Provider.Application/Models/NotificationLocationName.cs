3bb219289f5f200f35601d4402f5bf0982526729
Mike Wild <wildconsultingltd@googlemail.com>
2023-01-31 17:49:32 +0000

    Add new NotificationLocationName classes to support summary page

---
+++ src/Sfa.Tl.Find.Provider.Application/Models/NotificationLocationName.cs
@@ -0,0 +1,9 @@
+﻿namespace Sfa.Tl.Find.Provider.Application.Models;
+public class NotificationLocationName
+{
+    public int Id { get; init; }
+    
+    public string Name { get; init; }
+
+    public string Postcode { get; init; }
+}
---
+++ src/Sfa.Tl.Find.Provider.Application/Models/NotificationLocationNameDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Sfa.Tl.Find.Provider.Application.Models;
+public class NotificationLocationNameDto
+{
+    public int NotificationLocationId { get; init; }
+
+    public int? LocationId { get; init; }
+    
+    public string LocationName { get; init; }
+
+    public string Postcode { get; init; }
+}
