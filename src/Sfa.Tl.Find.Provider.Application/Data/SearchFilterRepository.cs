f8925b656824a8eafd42845b3503af9a83291665
Mike Wild <wildconsultingltd@googlemail.com>
2023-02-13 09:12:03 +0000

    Remove unused policy registry in search filters

--- src/Sfa.Tl.Find.Provider.Application/Data/SearchFilterRepository.cs
+++ src/Sfa.Tl.Find.Provider.Application/Data/SearchFilterRepository.cs
@@ -11,17 +11,14 @@ public class SearchFilterRepository : ISearchFilterRepository
     private readonly IDbContextWrapper _dbContextWrapper;
     private readonly IDynamicParametersWrapper _dynamicParametersWrapper;
     private readonly ILogger<SearchFilterRepository> _logger;
-    private readonly IReadOnlyPolicyRegistry<string> _policyRegistry;
 
     public SearchFilterRepository(
         IDbContextWrapper dbContextWrapper,
         IDynamicParametersWrapper dynamicParametersWrapper,
-        IReadOnlyPolicyRegistry<string> policyRegistry,
         ILogger<SearchFilterRepository> logger)
     {
         _dbContextWrapper = dbContextWrapper ?? throw new ArgumentNullException(nameof(dbContextWrapper));
         _dynamicParametersWrapper = dynamicParametersWrapper ?? throw new ArgumentNullException(nameof(dynamicParametersWrapper));
-        _policyRegistry = policyRegistry ?? throw new ArgumentNullException(nameof(policyRegistry));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
