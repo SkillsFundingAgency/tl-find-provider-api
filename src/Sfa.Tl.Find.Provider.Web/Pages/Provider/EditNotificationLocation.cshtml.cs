0a27fde36fefdbc10c59aa17671bafff2cd8d213
Mike Wild <wildconsultingltd@googlemail.com>
2023-02-17 17:10:24 +0000

    Post-merge fixes

--- src/Sfa.Tl.Find.Provider.Web/Pages/Provider/EditNotificationLocation.cshtml.cs
+++ src/Sfa.Tl.Find.Provider.Web/Pages/Provider/EditNotificationLocation.cshtml.cs
@@ -13,7 +13,7 @@ using Route = Sfa.Tl.Find.Provider.Application.Models.Route;
 
 namespace Sfa.Tl.Find.Provider.Web.Pages.Provider;
 
-[Authorize(nameof(PolicyNames.HasProviderAccount))]
+[Authorize(nameof(PolicyNames.IsProvider))]
 [ResponseCache(NoStore = true, Duration = 0, Location = ResponseCacheLocation.None)]
 public class EditNotificationLocationModel : PageModel
 {
