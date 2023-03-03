da35cd623bc7d6369e4bab05f67d6953b323e0de
Mike Wild <wildconsultingltd@googlemail.com>
2023-03-03 14:25:40 +0000

    Fix test broken in previous comnmit

--- src/Sfa.Tl.Find.Provider.Api.UnitTests/Validators/EmployerInterestInputModelValidatorTests.cs
+++ src/Sfa.Tl.Find.Provider.Api.UnitTests/Validators/EmployerInterestInputModelValidatorTests.cs
@@ -14,11 +14,12 @@ public class EmployerInterestInputModelValidatorTests
 
         var validator = new EmployerInterestInputModelValidator();
         var result = validator.Validate(target);
+
         result.Should().NotBeNull();
         result.Errors.Should().NotBeEmpty();
         result.Errors.Should()
             .Contain(x =>
-                x.PropertyName == "Location" &&
+                x.PropertyName == "Locations" &&
                 x.ErrorMessage == "One or more locations must be provided.");
     }
 }
