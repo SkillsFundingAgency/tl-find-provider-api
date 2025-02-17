9429ddd6518fb3917ea85078f212cc9e5403f46e
Ibai Gonzalez <Ibai.GONZALEZ@EDUCATION.GOV.UK>
2025-02-17 15:02:50 +0000

    Update security headers

---
+++ src/Sfa.Tl.Find.Provider.Web/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Sfa.Tl.Find.Provider.Web.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder SetStrictTransportSecurityHeader(this IApplicationBuilder app)
+    {
+        const int Days = 365;
+
+        return app.UseHsts(options =>
+        {
+            options.MaxAge(Days);
+            options.Preload();
+            options.IncludeSubdomains();
+        });
+    }
+
+    public static IApplicationBuilder SetConfigSecurityPolicyHeader(this IApplicationBuilder app)
+    {
+        string[] strictDynamicCustomSources = { "https:", "https://www.google-analytics.com/analytics.js", "https://www.googletagmanager.com/", "https://tagmanager.google.com/" };
+
+        return app.UseCsp(options => options
+            .ScriptSources(conf => conf
+                .StrictDynamic()
+                .CustomSources(strictDynamicCustomSources))
+            .ObjectSources(s => s.None()));
+    }
+}
